a314b6113fd781db3f9b62885db37d7cd8028358
Ayu85 <ayushsinghdalimss@gmail.com>
2024-12-23 12:46:29 +0530

    star rating done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Rating from './Questions/Star Rating/Rating'
 
 const App = () => {
   return (
-    <div className='bg-zinc-900 w-screen h-screen text-white'>App</div>
+    <div className='bg-zinc-900 flex justify-center items-center w-screen h-screen text-white'>
+      <Rating />
+    </div>
   )
 }
 
